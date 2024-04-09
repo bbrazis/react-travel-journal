473bfb473ee9c80f0204fcf85bf0d1e8b878aa44
bbrazis <web@broncowine.com>
2024-04-09 13:43:32 -0700

    new styles

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,16 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer>
-            <p className="legal">bbrazis. Scrimba Travel Journal solo project</p>
-            <div className="footer--nav">
-                <a href="https://www.bbrazis.com" target="_blank">
-                    bbrazis.com
-                </a>
-                <a href="https://github.com/bbrazis" target="_blank">
-                    github
-                </a>
+            <div className="container">
+                <p className="legal">bbrazis. Scrimba Travel Journal solo project</p>
+                <div className="footer--nav">
+                    <a href="https://www.bbrazis.com" target="_blank">
+                        bbrazis.com
+                    </a>
+                    <a href="https://github.com/bbrazis" target="_blank">
+                        github
+                    </a>
+                </div>
             </div>
         </footer>
     )
